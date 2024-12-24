1b10dc3774c47b4a4716c823eb21709e0a1a69ab
NNT9x <42613109+nnt9x@users.noreply.github.com>
2024-12-24 09:58:36 +0700

    - Cap nhat them so lan bam va total

--- app/src/main/java/com/bkacad/edu/nnt/dicegamed01/MainActivity.java
+++ app/src/main/java/com/bkacad/edu/nnt/dicegamed01/MainActivity.java
@@ -2,7 +2,9 @@ package com.bkacad.edu.nnt.dicegamed01;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +15,13 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imgDice;
+    private TextView tvTotal;
+
+    private Button btnReset;
+
+    // Khai bao count, total
+    private int total = 0;
+    private int count = 3; // toi da 3 lan
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,32 +35,53 @@ public class MainActivity extends AppCompatActivity {
         });
         // Bind id
         imgDice = findViewById(R.id.imgDice);
+        tvTotal = findViewById(R.id.tvTotal);
+        btnReset = findViewById(R.id.btnReset);
     }
 
     public void randomDice(View view) {
-        // Khi bam vao button dice -> ham nay se chay
-        int randomNum = (int) (Math.random() * 6) + 1;
-        // Dua vao gia tri => set hinh
-        switch (randomNum) {
-            case 1:
-                imgDice.setImageDrawable(getDrawable(R.drawable.dice1));
-                break;
-            case 2:
-                imgDice.setImageDrawable(getDrawable(R.drawable.dice2));
-                break;
-            case 3:
-                imgDice.setImageDrawable(getDrawable(R.drawable.dice3));
-                break;
-            case 4:
-                imgDice.setImageDrawable(getDrawable(R.drawable.dice4));
-                break;
-            case 5:
-                imgDice.setImageDrawable(getDrawable(R.drawable.dice5));
-                break;
-            case 6:
-                imgDice.setImageDrawable(getDrawable(R.drawable.dice6));
-                break;
+        // Nếu count > 0 -> được phép random, nếu bằng = 0 -> hiển thị total, đồng thời reset hình
+        if (count > 0) {
+            // Khi bam vao button dice -> ham nay se chay
+            int randomNum = (int) (Math.random() * 6) + 1;
+            total = total + randomNum;
+            // Dua vao gia tri => set hinh
+            switch (randomNum) {
+                case 1:
+                    imgDice.setImageDrawable(getDrawable(R.drawable.dice1));
+                    break;
+                case 2:
+                    imgDice.setImageDrawable(getDrawable(R.drawable.dice2));
+                    break;
+                case 3:
+                    imgDice.setImageDrawable(getDrawable(R.drawable.dice3));
+                    break;
+                case 4:
+                    imgDice.setImageDrawable(getDrawable(R.drawable.dice4));
+                    break;
+                case 5:
+                    imgDice.setImageDrawable(getDrawable(R.drawable.dice5));
+                    break;
+                case 6:
+                    imgDice.setImageDrawable(getDrawable(R.drawable.dice6));
+                    break;
+            }
+            // Tru count 1 donvi
+            count--;
+        }
+        if (count == 0) {
+            // count = 0
+            // hiển thị total, đồng thời reset hình
+            tvTotal.setText("Total: " + total);
+            btnReset.setVisibility(View.VISIBLE);
         }
     }
 
+    public void resetDice(View view) {
+        total = 0;
+        count = 3;
+        imgDice.setImageDrawable(getDrawable(R.drawable.dice0));
+        btnReset.setVisibility(View.INVISIBLE);
+        tvTotal.setText("Total: ?");
+    }
 }
